8af8f13a1950e9e0e2dc308228a38a88e00d3ac9
sunnykumarsam <101540270+sunnykumarsam@users.noreply.github.com>
2022-03-14 10:19:45 +0530

    exp1

---
+++ student.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+
+#include<conio.h>
+
+#include<malloc.h>
+ 
+struct student
+ 
+{
+   
+  char name[30];
+  
+   int roll;
+ 
+
+ };
+ 
+void main()
+ 
+{
+     struct student *p;
+
+     int i,n;
+ 
+    printf("Enter total no of student\n");
+ 
+    scanf("%d",&n);
+
+    p=(struct student *)malloc (n*sizeof(struct student));
+ 
+    for(i=0;i<n;i++)
+ 
+    {
+    
+     printf("enter roll \n");
+
+         scanf("%d",&p[i].roll);
+
+         printf("enter name \n");
+
+         scanf("%s",p[i].name);
+
+         
+     }
+  
+   for(i=0;i<n;i++)
+   
+  {
+       
+  printf("roll=%d\n",p[i].roll);
+  
+      printf("Name=%s\n",p[i].name);
+  
+   }
+
+ return 0; 
+   
+ }
+ 
+ 
+ 
+ 
+ 
+ 
